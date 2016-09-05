fda9e690dc208b9c9e872f615a14c5c097227b59
Jon Pipitone <jon@pipitone.ca>
2016-09-05 18:00:11 -0400

    Structural MRI data and switched subject ID format

--- data/clean.R
+++ data/clean.R
@@ -14,12 +14,12 @@ dem$age = as.numeric(as.character(dem$age))
 dem$ethnicity = factor(dem$ethnicity,levels=c("Cauc","AA","As","In","Other"))
 dem$sex = factor(dem$sex, levels=c(0,1), labels=c("Male","Female"))
 dem$dx = factor(dem$dx, levels=c(0,1), labels=c("Control","Case"))
+dem$subject_ID = gsub(dem$subject_ID,pattern="SUB_",replacement="subject")
 
 gen[gen==""] = NA
 gen[gen=="missing"] = NA
 gen[gen=="9999"] = NA
 gen$genotype = factor(gen$genotype, levels=c(0,1,2), labels=c("AA","AG","GG"))
-gen$subID = gsub(gen$subID,pattern="subject",replacement="SUB_")
 
 cog[cog==""] = NA
 cog[cog=="missing"] = NA
@@ -27,7 +27,6 @@ cog[cog=="9999"] = NA
 cog$cog1 = as.numeric(as.character(cog$cog1))
 cog$cog2 = as.numeric(as.character(cog$cog2))
 cog$cog3 = as.numeric(as.character(cog$cog3))
-cog$subID = gsub(cog$subID,pattern="subject",replacement="SUB_")
 
 
 all = merge(dem,
